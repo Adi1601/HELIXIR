9bbc20fc4ac686fb7be0e40f638075e097e194bb
Alexandra-Diana Nedelcu <diana.nedelcu@tum.de>
2021-07-20 20:36:57 +0000

    Upload New File

---
+++ sebaproject/backend/routes/appointment.js
@@ -0,0 +1,42 @@
+//This route is used for use case 2: booking appointments
+const router = require('express').Router();
+let Appointment =  require('../models/appointment.model');
+const jwt = require('jsonwebtoken');
+
+const {
+    authorizeToken
+} = require('./security/authorization_token.js');
+
+//get localhost:5000/appointment
+router.route('/').get(authorizeToken, (req,res)=> {
+	Appointment.find()
+    	.then(appointments => res.json(appointments))
+    	.catch(err => res.status(400).json('Error: ' + err));  	
+});
+
+//post localhost:5000/appointment/appointmentCreate
+router.route('/appointmentCreate').post(authorizeToken, (req,res) => {
+    // Creates a new record from a submitted form
+    var newappointment = new Appointment({
+		name: req.body.name,
+		email: req.body.email,
+		doctor_name: req.body.doctor_name,
+		start_time: req.body.start_time,
+		motive: req.body.motive
+    });
+
+    // and saves the record to the data base
+    newappointment.save()//mongoose method and returns a then/catch
+    	.then(() => res.json('New appointment created!'))//what is returned on the screen
+    	.catch(err => res.status(400).json('Error: ' + err));//cath error if there process doesnt work
+});
+
+//Delete an appointment
+//delete localhost:5000/appointment/_id
+router.route('/:id').delete(authorizeToken, (req, res) => { 
+  Appointment.findByIdAndDelete(req.params.id) //mongoose method and returns a then/catch
+      .then(appointment => res.json(appointment +  'was deleted!'))  //what is returned on the screen
+      .catch(err => res.status(400).json('Error: ' + err));//cath error if there process doesnt work
+});
+
+module.exports = router;
